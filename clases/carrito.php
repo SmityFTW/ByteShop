8a7752bdf52f5a1d9c2beb1e4e3169e846d8c8bb
SmityFTW <ladenc168@gmail.com>
2024-11-23 21:38:14 -0600

    actualizacion de emails smtp 	modified:   checkout.php 	modified:   clases/Array 	modified:   clases/actualizar_carrito.php 	modified:   clases/captura.php 	modified:   clases/carrito.php 	modified:   clases/enviar_email.php 	deleted:    completado.html 	modified:   completado.php 	modified:   config/config.php 	modified:   config/database.php 	modified:   contact.php
    	modified:   completado.php

--- clases/carrito.php
+++ clases/carrito.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require '../config/config.php';
@@ -28,34 +27,3 @@ if (isset($_POST['id'])) {
 }
 
 echo json_encode($datos);
-=======
-<?php
-
-require '../config/config.php';
-
-if (isset($_POST['id'])) {
-
-    $id = $_POST['id'];
-    $token = $_POST['token'];
-
-    $token_tmp = hash_hmac('sha1', $id, KEY_TOKEN);
-
-    if ($token == $token_tmp) {
-
-        if(isset($_SESSION['carrito']['productos'][$id])){
-            $_SESSION['carrito']['productos'][$id] += 1;
-        } else {
-            $_SESSION['carrito']['productos'][$id] = 1;
-        }
-
-        $datos['numero'] = count($_SESSION['carrito']['productos']);
-        $datos['ok'] = true;
-    } else {
-        $datos['ok'] = false;
-    }
-} else {
-    $datos['ok'] = false;
-}
-
-echo json_encode($datos);
->>>>>>> 3d0a4ac568ce5cfeb76d2afadfeba4bf8e8e20d5
--- clases/enviar_email.php
+++ clases/enviar_email.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 use PHPMailer\PHPMailer\{PHPMailer, SMTP, Exception};
@@ -48,55 +47,4 @@ try {
 } catch (Exception $e) {
     echo "Error al enviar el correo electronico de la compra: {$mail->ErrorInfo}";
     
-=======
-<?php
-
-use PHPMailer\PHPMailer\{PHPMailer, SMTP, Exception};
-
-require '../phpmailer/src/PHPMailer.php';
-require '../phpmailer/src/SMTP.php';
-require '../phpmailer/src/Exception.php';
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //SMTP::DEBUG_OFF;  //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'termuxcodes@gmail.com';                     //SMTP username
-    $mail->Password   = '';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('termuxcodes@gmail.com', 'ByteShop');
-    $mail->addAddress('ladenc168@gmail.com');     //Add a recipient
-
-    //Attachments
-   // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Detalles de su compra';
-
-    $cuerpo = '<h4>Gracias por su compra</h4>';
-    $cuerpo .= '<p>El ID de su compra es <b>'. $id_transaccion .'</b></p>' ;
-
-
-    $mail->Body    = utf8_decode($cuerpo);
-    $mail->AltBody = 'Le enviamos los detalles de su compra.';
-
-    $mail->setLanguage('es', '../phpmailer/language/phpmailer.lang-es.php');
-
-    $mail->send();
-    echo 'el mensaje fue enviado';
-    
-} catch (Exception $e) {
-    echo "Error al enviar el correo electronico de la compra: {$mail->ErrorInfo}";
-    
->>>>>>> 3d0a4ac568ce5cfeb76d2afadfeba4bf8e8e20d5
 }
