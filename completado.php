ae7dd24ec7e6b2e60dc2632ef21592fb4e7ac85d
SmityFTW <ladenc168@gmail.com>
2024-11-26 16:47:36 -0600

    actualizacion de pago y emails

--- completado.php
+++ completado.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require 'config/config.php';
--- scripts/ajax_cart.js
+++ scripts/ajax_cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function addProducto(id, token){
     let url = 'clases/carrito.php'
     let formData = new FormData()
@@ -16,23 +15,4 @@ function addProducto(id, token){
         elemento.innerHTML = data.numero
       }
     })
-=======
-function addProducto(id, token){
-    let url = 'clases/carrito.php'
-    let formData = new FormData()
-    formData.append('id', id)
-    formData.append('token', token)
-
-    fetch(url, {
-      method: 'POST',
-      body: formData,
-      mode: 'cors'
-    }).then(response => response.json())
-    .then(data => {
-      if(data.ok){
-        let elemento = document.getElementById("num_cart")
-        elemento.innerHTML = data.numero
-      }
-    })
->>>>>>> 3d0a4ac568ce5cfeb76d2afadfeba4bf8e8e20d5
   }
